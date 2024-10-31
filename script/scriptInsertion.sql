7c487232b7eef6c6f4cdbabe71d6ce0185c93168
Najib BARBARA <najib.barbara@etu.uca.fr>
2024-10-31 19:08:03 +0100

    Mise à jour de 'script/scriptInsertion.sql'

--- script/scriptInsertion.sql
+++ script/scriptInsertion.sql
@@ -6,5 +6,11 @@ INSERT INTO Fournisseur VALUES
 ('FRF00003', 'FranceSonor', 69007, 'Lyon', 'France', 'support@franceSonor.fr', 'Rue de Richelieu', 33712345678),
 ('FRF00004', 'PasJBL', 33000, 'Bordeaux', 'France', 'contact@jbl.fr', 'Rue Sainte-Catherine', 33598761234),
 ('FRF00005', 'PrestigeAudio', 31000, 'Toulouse', 'France', 'service@prestigeaudio.fr', 'Place du Capitole', 33765432109),
-('USF00001', 'ConcertEquip', 10001, 'New-York', 'Etats-unis', 'concertEquipUSA@fresco.us', 'BradFord avanue', );
+('USF00001', 'ConcertEquip', 10001, 'New-York', 'Etats-unis', 'concertEquipUSA@fresco.us', 'BradFord avanue', null);
+
+
+INSERT INTO Stock VALUES 
+('FRS00001', '1 rue Lamartine', 63100, 'Clermont-Fd', 'France'),
+('FRS00002', '6 rue Jean-Jaurès', 70123, 'Paries', 'France'),
+(),
 
