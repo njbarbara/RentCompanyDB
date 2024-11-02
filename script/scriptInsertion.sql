0d8be4b70454ea237f2f62b02deb275ad6a98ebe
Najib BARBARA <najib.barbara@etu.uca.fr>
2024-11-02 18:17:37 +0100

    Mise à jour de 'script/scriptInsertion.sql'

--- script/scriptInsertion.sql
+++ script/scriptInsertion.sql
@@ -1,16 +1,19 @@
 /*Ce script n'est pas abouti et contient peut-être des erreurs (!!!)*/
 
 INSERT INTO Fournisseur VALUES 
-('FRF00001', 'Focal', 70123, 'Paris', 'France', 'focal-pro@dmail.fr', 'Rue du sénat', 33123456789),
-('FRF00002', 'Bose', 63100, 'Clermont', 'France', 'Bose@neuf.com', 'Rue marcel du chemin ', 33698765432),
-('FRF00003', 'FranceSonor', 69007, 'Lyon', 'France', 'support@franceSonor.fr', 'Rue de Richelieu', 33712345678),
-('FRF00004', 'PasJBL', 33000, 'Bordeaux', 'France', 'contact@jbl.fr', 'Rue Sainte-Catherine', 33598761234),
-('FRF00005', 'PrestigeAudio', 31000, 'Toulouse', 'France', 'service@prestigeaudio.fr', 'Place du Capitole', 33765432109),
-('USF00001', 'ConcertEquip', 10001, 'New-York', 'Etats-unis', 'concertEquipUSA@fresco.us', 'BradFord avanue', null);
+('FRF00001', 'Focal', 70123, 'Paris', 'France', 'focal-pro@dmail.fr', 'Rue du sénat', '33123456789'),
+('FRF00002', 'Bose', 63100, 'Clermont', 'France', 'Bose@neuf.com', 'Rue marcel du chemin', '33698765432'),
+('FRF00003', 'FranceSonor', 69007, 'Lyon', 'France', 'support@franceSonor.fr', 'Rue de Richelieu', '33712345678'),
+('FRF00004', 'PasJBL', 33000, 'Bordeaux', 'France', 'contact@jbl.fr', 'Rue Sainte-Catherine', '33598761234'),
+('FRF00005', 'PrestigeAudio', 31000, 'Toulouse', 'France', 'service@prestigeaudio.fr', 'Place du Capitole', '33765432109'),
+('USF00001', 'ConcertEquip', 10001, 'New-York', 'Etats-unis', 'concertEquipUSA@fresco.us', 'BradFord avenue', '5556789012'),
+('USF00002', 'StageCraft Solutions', 90001, 'Los Angeles', 'Etats-unis', 'stageCraft@fedex.us', 'Rodeo street', '5551234567'),
+('USF00003', 'SoundWave Equipment', 77001, 'Houston', 'Etats-unis', 'soundwave@houstonMail.us', 'Main street', '5556789012'),
+('CNF00001', 'Toronto Evenement', NULL, 'Toronto', 'Canada', 'torontoEvenement@gmail.com', 'Yonge Street', '5556040123'),
+('JPF00001', 'Tokyo Disco', 1000001, 'Tokyo', 'Japon', 'tokyoDisco@gmail.com', 'Yanaka Ginza', '0312345678');
 
 
 INSERT INTO Stock VALUES 
 ('FRS00001', '1 rue Lamartine', 63100, 'Clermont-Fd', 'France'),
-('FRS00002', '6 rue Jean-Jaurès', 70123, 'Paries', 'France'),
-(),
-
+('FRS00002', '6 rue Jean-Jaurès', 70123, 'Paris', 'France'),
+('FRS00003'),
