1fb165a0b723a6cb4842ef66283e8a805d8bfa8f
aron.agocs <aron.agocs@etu.uca.fr>
2024-11-29 16:32:41 +0100

    insertError

--- script/scriptInsertionErreur.sql
+++ script/scriptInsertionErreur.sql
@@ -6,3 +6,18 @@ A faire :
 
 INSERT INTO Fournisseur VALUES 
 ('FRF00001', 'Focal', 70123, 'Paris', 'France', NULL, 'Rue du sénat', NULL);
+
+
+INSERT INTO Client VALUES
+('BEC00001', 'WeAreOne.World BVBA', 'Korte Vlierstraat 6', NULL, 'Antwerpen', 'Belgique', NULL, NULL);
+
+
+INSERT INTO Location VALUES
+('LCFR0001', '2024-07-02', '2024-06-25', 1567.89, 'FRC00001'),
+('LCFR0001', '2024-06-25', '2024-07-02', -1567.89, 'FRC00001');
+
+
+INSERT INTO Produit VALUES
+('PRD00001', 'Flexible Array F1 modèle 812', 'Enceinte ', 'Bose', -1349, 'SPE00001');
+
+
